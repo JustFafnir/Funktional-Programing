6952e11454b5f66a2d84ff423f2b669e2e5f4d87
JustFafnir <59540172+JustFafnir@users.noreply.github.com>
2020-01-23 20:19:32 +0200

    Add files via upload

---
+++ 1.st Task/src/PerfectNumberTest.java
@@ -0,0 +1,57 @@
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PerfectNumberTest {
+
+	@Test
+	public void test6Perfect() {
+		assertEquals(PerfectNumber.STATE.PERFECT, PerfectNumber.process(6));
+	}
+
+	@Test
+	public void test8Deficient() {
+		assertEquals(PerfectNumber.STATE.DEFICIENT, PerfectNumber.process(8));
+	}
+
+	@Test
+	public void test20Abundant() {
+		assertEquals(PerfectNumber.STATE.ABUNDANT, PerfectNumber.process(20));
+	}
+
+	@Test
+	public void test16DeficientWithIntSqrt() {
+		assertEquals(PerfectNumber.STATE.DEFICIENT, PerfectNumber.process(16));
+	}
+
+	@Test
+	public void test1Deficient() {
+		assertEquals(PerfectNumber.STATE.DEFICIENT, PerfectNumber.process(1));
+	}
+
+	@Test
+	public void testDivisors1() {
+		Object[] expected = new Integer[] { 1 };
+		int n = 1;
+		assertArrayEquals(expected, PerfectNumber.divisors(n).toArray());
+	}
+
+	@Test
+	public void testDivisors6() {
+		Object[] expected = new Integer[] { 1, 2, 3, 6 };
+		int n = 6;
+		assertArrayEquals(expected, PerfectNumber.divisors(n).toArray());
+	}
+
+	@Test
+	public void testDivisors8() {
+		Object[] expected = new Integer[] { 1, 2, 4, 8 };
+		int n = 8;
+		assertArrayEquals(expected, PerfectNumber.divisors(n).toArray());
+	}
+
+
+	
+	
+}
